375acb013cf77c183bff8336f0d006e7271494bb
子瞻 Luci <22126563+LuciNyan@users.noreply.github.com>
2023-02-24 18:42:17 +0800

    chore: add alias

--- src/operator.ts
+++ src/operator.ts
@@ -1,13 +1,16 @@
 import { UnionToIntersection } from './utils'
 
-export function or<T extends any[]>(...guards: { [K in keyof T]: (x: unknown) => x is T[K] }): (x: unknown) => x is T[number] {
+export function union<T extends any[]>(...guards: { [K in keyof T]: (x: unknown) => x is T[K] }): (x: unknown) => x is T[number] {
     return (x: unknown): x is T[number] => {
         return guards.some((guard) => guard(x))
     }
 }
 
-export function and<T extends any[]>(...guards: { [K in keyof T]: (x: unknown) => x is T[K] }): (x: unknown) => x is UnionToIntersection<T[number]> {
+export function intersection<T extends any[]>(...guards: { [K in keyof T]: (x: unknown) => x is T[K] }): (x: unknown) => x is UnionToIntersection<T[number]> {
     return (x: unknown): x is UnionToIntersection<T[number]> => {
         return guards.every((guard) => guard(x));
     };
 }
+
+export const or = union
+export const and = intersection
