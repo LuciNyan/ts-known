766b3dafde1b836156ec2f02dd49cd27b198f4e3
子瞻 Luci <22126563+LuciNyan@users.noreply.github.com>
2023-02-24 17:47:43 +0800

    chore: add operator, make tests

---
+++ src/make.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest'
+import { make } from './make'
+import { isString, isNumber, isBoolean } from './base'
+
+describe('make', () => {
+    it('should return a function that checks if an object has properties guarded by given guards', () => {
+        const guardByProperty = {
+            foo: isString,
+            bar: isNumber,
+            baz: isBoolean,
+        }
+        const guardObject = make(guardByProperty)
+
+        expect(guardObject({ foo: 'hello', bar: 123, baz: false })).toBe(true)
+        expect(guardObject({ foo: 'world', bar: '123', baz: false })).toBe(false)
+        expect(guardObject({ foo: 'hello', bar: 123 })).toBe(false)
+    })
+})
---
+++ src/operator.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from 'vitest'
+import { or, and } from './operator'
+
+describe('or', () => {
+    it('should return true if value matches any of the guards', () => {
+      const guard = or(isString, isNumber, isBoolean);
+      expect(guard('foo')).toBe(true);
+      expect(guard(123)).toBe(true);
+      expect(guard(true)).toBe(true);
+    });
+  
+    it('should return false if value does not match any of the guards', () => {
+      const guard = or(isString, isNumber, isBoolean);
+      expect(guard(undefined)).toBe(false);
+      expect(guard(null)).toBe(false);
+      expect(guard({})).toBe(false);
+      expect(guard([])).toBe(false);
+    });
+});
+  
+describe('and', () => {
+    it('should return true if value matches all of the guards', () => {
+      const guard = and(isObject, hasProperty('name', isString));
+      expect(guard({ name: 'John' })).toBe(true);
+    });
+  
+    it('should return false if value does not match all of the guards', () => {
+      const guard = and(isObject, hasProperty('name', isString));
+      expect(guard(undefined)).toBe(false);
+      expect(guard(null)).toBe(false);
+      expect(guard({ name: 123 })).toBe(false);
+      expect(guard({ age: 30 })).toBe(false);
+    });
+});
+  
