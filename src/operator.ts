9c95c2693b93df3ed2a7da6020067faf2f4cb281
子瞻 Luci <22126563+LuciNyan@users.noreply.github.com>
2023-02-28 16:14:08 +0800

    feat: arrayOf

--- src/operator.ts
+++ src/operator.ts
@@ -16,5 +16,15 @@ export function intersection<T extends any[]>(
   }
 }
 
+export function arrayOf<T extends unknown[]>(...guards: { [K in keyof T]: Guard<T[K]> }): Guard<T> {
+  return function (x: unknown): x is T {
+    if (!Array.isArray(x)) {
+      return false
+    }
+
+    return x.every((item) => guards.some(guard => guard(item)))
+  }
+}
+
 export const or = union
 export const and = intersection
